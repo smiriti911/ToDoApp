ae3bed611ff91b73adbc28b47e55dfef5ade8bbc
smiriti <148301685+smiriti911@users.noreply.github.com>
2025-03-16 23:30:06 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -74,7 +74,7 @@ export default function Login() {
           </button>
         </form>
         <p className="mt-4 text-center text-black">
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <Link href="/signup" className="text-lime-600 hover:underline">
             Sign up
           </Link>
